3648d5f3f5eed9acdf9a8bdeb1fc10e16048c6c2
Pires Puc <matheuspiressdev@gmail.com>
2025-03-19 20:01:20 -0300

    Criação do projeto

---
+++ src/main/java/br/com/ToCare/ToCareApplication.java
@@ -0,0 +1,13 @@
+package br.com.ToCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ToCare/ToCareApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ToCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
